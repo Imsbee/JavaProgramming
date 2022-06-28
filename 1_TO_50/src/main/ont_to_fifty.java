12005279d2c09b087d43998674e89156cfcf0088
Imsbee <aosj2131@naver.com>
2022-06-28 22:10:54 +0900

    day02

--- 1_TO_50/src/main/ont_to_fifty.java
+++ 1_TO_50/src/main/ont_to_fifty.java
@@ -5,51 +5,55 @@ import javax.swing.*;
 import window.LocationCenter;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.Random;
 
 public class ont_to_fifty extends JFrame {
+	JButton[] btns = new JButton[25];
+	Random r = new Random();
+	Boolean[] is_status = new Boolean[25];
+	int[] number = new int[25];
+	int score = 0;
+	int i = 0;
+
 	public ont_to_fifty() {
 		int w = 500;
 		int h = 500;
 		int num = 0;
-		int i = 0;
-		int score = 0;
 		setLayout(new GridLayout(5, 5));
-		JButton[] btns = new JButton[25];
-		Random r = new Random();
-		Boolean[] is_status = new Boolean[25];
 		Arrays.fill(is_status, false); // boolean형 배열인 is_status의 모든 방을 false로 초기화
-		
-		while(i < 25) {	// 1~25까지 버튼 생성
+
+		// 1~25까지 버튼 생성
+		while (i < 25) {
 			num = r.nextInt(25);
 			btns[i] = new JButton(num + 1 + "");
-			if(is_status[num] == false) {
+			if (is_status[num] == false) {
 				is_status[num] = true;
 				add(btns[i]);
 				i++;
-
 			}
 		}
-		
-		while(score < 25) { // 게임 실행 부분
-			btns[score].addActionListener(event -> {
-				btns[score].setVisible(false);
-				
-					
-			});
+
+		// 숫자 방을 1~25로 초기화
+		for (i = 1; i <= 25; i++) {
+			number[i - 1] = i;
 		}
-		
+
+		// 창 관련 설정
 		setTitle("1 TO 50");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		LocationCenter lc = new LocationCenter(w, h);
 		setBounds(lc.getX(), lc.getY(), w, h);
 		setVisible(true);
-		
-		
+
 	}
+
+	
 
 	public static void main(String[] args) {
 		new ont_to_fifty();
 	}
+
 }
